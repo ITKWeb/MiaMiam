f68c5bd90fdad48af2c9360e89053ab2556e95a3
jeremy-flusin <flusin.jeremy@gmail.com>
2013-10-08 13:55:39 +0200

    Update firstPageController.js

--- public/javascripts/controllers/firstPageController.js
+++ public/javascripts/controllers/firstPageController.js
@@ -2,12 +2,11 @@ app.controller('firstPageController', ['$scope', 'Network', '$location', functio
 
 
     $scope.clickOnNewMeal = function() {
-		$location.path('/meal');
+        $location.path('/meal');
     }
 
-	
-	$scope.clickSignUp = function() {
-		$location.path("/signup");
-	}
+    $scope.clickSignUp = function() {
+        $location.path("/signup");
+    }
 
 }]);
