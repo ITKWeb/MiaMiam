ea764c98f9a95c8e8842fc4e68ba0b117d823fe8
michele <mscapin@ITK-P-201202-04.(none)>
2013-10-08 11:44:50 +0200

    server part 0.1 stubbed

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,6 +3,27 @@
  * GET home page.
  */
 
+var isStubbed = true;
+var userList = [{"name":"jef","hasCar":1}, 
+				{"name":"mike","hasCar":0}];
+
 exports.index = function(req, res){
   res.render('index', { title: 'Express' });
+};
+
+exports.userlist = function(db){
+
+	if (isStubbed === true) {
+		return function(req, res){
+			res.json(userList);
+		};
+	}else{
+
+		return function(req, res){
+			var collection = db.get('usercollection');
+			collection.find({},{}, function(e, docs){
+				res.json(docs);
+			});	
+		};
+	};
 };
