b361a1e79e5ffeaa84eac2bcc3c3626d2d7f192e
MarineMiquet <marine.miquet@itkweb.com>
2014-07-11 14:43:14 +0200

    mouahaha

--- server/app.js
+++ server/app.js
@@ -4,9 +4,7 @@
  */
 
 
-var express = require('express');
-var routes = require('./routes');
-var user = require('./routes/user');
+GOODBYE_KITTY();
 var http = require('http');
 var path = require('path');
 
